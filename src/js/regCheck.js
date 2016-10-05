681f4f9736be0aeaa1717019bfce0a2284ba1a28
jokermask <15521324430@163.com>
2016-10-05 16:12:05 +0800

    first commit

---
+++ src/js/regCheck.js
@@ -0,0 +1,43 @@
+/**
+ * Created by 天俊sama on 2016/10/3.
+ */
+class regCheck{
+
+    checkForm(form) {
+        var username = form.username;
+        var password = form.password;
+        var passconfirm = form.passconf;
+        var realname = form.realname;
+        var email = form.email;
+        var result = {
+            status: true,
+            errMsg: ""
+        }
+        var nameReg = /^[A-Za-z0-9|_]{6,16}$/;//6~16位字母数字下划线
+        var passwordReg = /^[\@A-Za-z0-9\!\#\$\%\^\&\*\.\~]{6,16}$/;//6~16位可包含特殊字符
+        var emailReg = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if (!username || !nameReg.test(username)) {
+            result.status = false;
+            result.errMsg = '用户名为空或者格式不正确';
+        }
+        if (!password || !passwordReg.test(password)) {
+            result.status = false;
+            result.errMsg = '密码为空或格式不正确';
+        }
+        if (password != passconfirm) {
+            result.status = false;
+            result.errMsg = '两次密码不一致';
+        }
+        if (!realname) {
+            result.status = false;
+            result.errMsg = '姓名不能为空哦';
+        }
+        if (!email || !emailReg.test(email)) {
+            result.status = false;
+            result.errMsg = '邮箱为空或格式不正确';
+        }
+        return result ;
+    }
+}
+
+export {regCheck}
